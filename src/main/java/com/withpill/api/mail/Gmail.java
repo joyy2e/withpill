1a13f08becf6d617bd9fa9350f20b4ffccabbf48
joyfulwave <60209293+joyy2e@users.noreply.github.com>
2023-05-16 08:26:58 +0900

    Update Gmail.java

--- src/main/java/com/withpill/api/mail/Gmail.java
+++ src/main/java/com/withpill/api/mail/Gmail.java
@@ -15,8 +15,8 @@ import javax.mail.internet.MimeMessage;
 import com.withpill.web.account.dto.FindDto;
 
 public class Gmail {
-	private static String user = "openkufo@gmail.com"; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
-    private static String password = "jzmiawbkwshwzbcb";   // 패스워드
+	private static String user = ""; // 네이버일 경우 네이버 계정, gmail경우 gmail 계정
+    private static String password = "";   // 패스워드
 	public static void send() {
 
         // SMTP 서버 정보를 설정한다.
